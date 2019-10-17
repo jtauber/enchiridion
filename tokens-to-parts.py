a8c0ed10b2bb269812747f2a7368e3654a5fec7f
James Tauber <jtauber@jtauber.com>
2019-10-17 03:23:28 -0400

    static html generation based on AF

--- tokens-to-parts.py
+++ tokens-to-parts.py
@@ -5,7 +5,9 @@ with open("analysis/enchiridion.txt") as f:
     for line in f:
         ref, text, word, norm = line.strip().split()
         c, v, t = ref.split(".")
-        cv = f"{c}.{v}"
+        if int(c) == 0:
+            continue
+        cv = f"{int(c)}.{int(v)}"
         if cv != prev:
             if prev is not None:
                 print()
